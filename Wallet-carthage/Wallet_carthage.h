ed4a950e259ab3818fe6b6362c7f77094a7e23a1
Kohei Nakashima <nakashima_k@mediaxis.jp>
2018-04-11 11:25:05 +0900

    update for carthage

---
+++ Wallet-carthage/Wallet_carthage.h
@@ -0,0 +1,18 @@
+//
+//  Wallet_carthage.h
+//  Wallet-carthage
+//
+//  Created by Kohei Nakashima on 2018/04/11.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wallet_carthage.
+FOUNDATION_EXPORT double Wallet_carthageVersionNumber;
+
+//! Project version string for Wallet_carthage.
+FOUNDATION_EXPORT const unsigned char Wallet_carthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wallet_carthage/PublicHeader.h>
+
+
